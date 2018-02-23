32745b0abc16de3a09d31fd7ca2547b7af39fe50
owenmyers <owendalemyers@gmail.com>
2018-02-23 11:34:14 -0500

    Added a mostly blank template file

---
+++ template.py
@@ -0,0 +1,7 @@
+from dragonfly import (Grammar, 
+                       AppContext, 
+                       MappingRule, 
+                       Dictation, 
+                       InegerRef)
+
+vscode_context = AppContext(executable="putty")
